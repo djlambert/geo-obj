5464814effec3c6c19b5b8544bd184f2765caf06
Derek J. Lambert <dlambert@dereklambert.com>
2016-05-20 13:31:50 -0500

    Remove unused methods

--- lib/Data/Generator/SimpleArray.php
+++ lib/Data/Generator/SimpleArray.php
@@ -112,41 +112,6 @@ class SimpleArray implements GeneratorInterface
     }
 
     /**
-     * @param array $value
-     *
-     * @return bool
-     */
-    private function isObjectArray(array $value)
-    {
-        if (! $this->isSequential($value)) {
-            return false;
-        }
-
-        return count($value) === count(array_filter($value, 'isObject'));
-    }
-
-    /**
-     * @param array $value
-     *
-     * @return bool
-     */
-    private function isPoint(array $value)
-    {
-        if (! $this->isSequential($value)) {
-            return false;
-        }
-
-        $count = count($value);
-
-        if ($count < 2 || $count > 4) {
-            return false;
-        }
-
-
-        return $count === count(array_filter($value, 'isObject'));
-    }
-
-    /**
      * @param array $array
      *
      * @return bool
